ca9a7a5f4c62f4a685d5ad8378e71a6fb998d223
Vlad Alex <vladalex997@gmail.com>
2023-03-28 15:58:51 +0300

    Add project files.

---
+++ Transport Weekend/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace Transport_Weekend
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void MsgBox(String ex, Page pg, Object obj)
+        {
+            string s = "<SCRIPT language='javascript'>alert('" + ex.Replace("\r\n", "\\n").Replace("'", "") + "'); </SCRIPT>";
+            Type cstype = obj.GetType();
+            ClientScriptManager cs = pg.ClientScript;
+            cs.RegisterClientScriptBlock(cstype, s, s.ToString());
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+
+            if (inputId.Value == "1")
+            {
+                MsgBox("! your message !", this.Page, this);
+            }
+            else
+            {
+                Response.Redirect("~/Subordonates.aspx");
+            }
+
+           
+
+            
+        }
+    }
+}
