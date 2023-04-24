6b192e84675710db3a56fc0cb5c8c49566ba01b9
Vlad Alex <vladalex997@gmail.com>
2023-04-24 13:55:24 +0300

    modificat superior sa fie nume

---
+++ Transport Weekend/Email.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+
+namespace Transport_Weekend
+{
+    public class Email
+    {
+        public bool Send(string text, string subiect, string AdressTo, string AdressCc)
+        {
+            List<string> To = new List<string>();
+            List<string> Cc = new List<string>();
+            string[] ato = AdressTo.Split(';');
+            string[] acc = AdressCc.Split(';');
+            foreach (string adresa in ato)
+            {
+                if (IsValidEmail(adresa))
+                    To.Add(adresa);
+            }
+            foreach (string adresa in acc)
+            {
+                if (IsValidEmail(adresa))
+                    Cc.Add(adresa);
+            }
+
+            SmtpClient client = new SmtpClient();
+            client.Port = 25;
+            client.Host = "mail.ustunberkteknik.com";
+            client.EnableSsl = false;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("lectra@ustunberkteknik.com", "jh0SjQC74y");
+
+            MailMessage mm = new MailMessage();
+            mm.IsBodyHtml = true;
+            foreach (string a in To)
+            {
+                mm.To.Add(a);
+            }
+            foreach (string a in Cc)
+            {
+                mm.CC.Add(a);
+            }
+            mm.From = new MailAddress("lectra@ustunberkteknik.com", "Martur Announcement System");
+            mm.Subject = subiect;
+            mm.IsBodyHtml = true;
+            mm.Body = text;
+            mm.BodyEncoding = UTF8Encoding.UTF8;
+            mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- Transport Weekend/Subordinates.aspx.cs
+++ Transport Weekend/Subordinates.aspx.cs
@@ -51,7 +51,9 @@ namespace Transport_Weekend
 
         public void RefreshGridAll()
         {
-            string loggedin = Request.Cookies["userdata"].Value;
+            string loggedinID = Request.Cookies["userdata"].Value;
+            GetUserName getUserName = new GetUserName();
+            string loggedin = getUserName.GetName(loggedinID);
             try
             {
                 string active = "ACTIVE";
@@ -79,7 +81,9 @@ namespace Transport_Weekend
 
         public void RefreshGridProgrammed()
         {
-            string loggedin = Request.Cookies["userdata"].Value;
+            string loggedinID = Request.Cookies["userdata"].Value;
+            GetUserName getUserName = new GetUserName();
+            string loggedin = getUserName.GetName(loggedinID);
             try
             {
                 string active = "ACTIVE";
@@ -215,6 +219,67 @@ namespace Transport_Weekend
             databaseObject.CloseConnection();
         }
 
+        public static string getHtml(DataTable table1)
+        {
+            try
+            {
+                string messageBody = "<font>The following people are going to come on Martur site for different jobs. Please check and be sure about HSE training status and validity date: </font><br><br>";
+
+                if (table1.Rows.Count == 0)
+                    return messageBody;
+                string htmlTableStart = "<table style=\"border-collapse:collapse; text-align:center;\" >";
+                string htmlTableEnd = "</table>";
+                string htmlHeaderRowStart = "<tr style =\"background-color:#6FA1D2; color:#ffffff;\">";
+                string htmlHeaderRowEnd = "</tr>";
+                string htmlTrStart = "<tr style =\"color:#555555;\">";
+                string htmlTrEnd = "</tr>";
+                string htmlTdStart = "<td style=\" border-color:#5c87b2; border-style:solid; border-width:thin; padding: 5px;\">";
+                string htmlTdEnd = "</td>";
+
+                messageBody += htmlTableStart;
+                messageBody += htmlHeaderRowStart;
+                messageBody += htmlTdStart + "Id Record " + htmlTdEnd;
+                messageBody += htmlTdStart + "Nume si Prenume " + htmlTdEnd;
+                messageBody += htmlTdStart + "Carte de identitate " + htmlTdEnd;
+                messageBody += htmlTdStart + "Status Persoana " + htmlTdEnd;
+                messageBody += htmlTdStart + "Ora Programata Venire " + htmlTdEnd;
+                messageBody += htmlTdStart + "Instruire valabila pana la " + htmlTdEnd;
+                messageBody += htmlTdStart + "Status Instruire " + htmlTdEnd;
+                messageBody += htmlTdStart + "Instruit De " + htmlTdEnd;
+
+                messageBody += htmlHeaderRowEnd;
+
+                //foreach (DataRow Row in dataSet.Tables[0].Rows)
+                //{
+                //    messageBody = messageBody + htmlTrStart;
+                //    messageBody = messageBody + htmlTdStart + Row["fieldName"] + htmlTdEnd;
+                //    messageBody = messageBody + htmlTrEnd;
+                //}
+                //messageBody = messageBody + htmlTableEnd;
+
+                for (int i = 0; i <= table1.Rows.Count - 1; i++)
+                {
+                    messageBody = messageBody + htmlTrStart;
+                    messageBody = messageBody + htmlTdStart + table1.Rows[i][0] + htmlTdEnd;
+                    messageBody = messageBody + htmlTdStart + table1.Rows[i][1] + htmlTdEnd;
+                    messageBody = messageBody + htmlTdStart + table1.Rows[i][2] + htmlTdEnd;
+                    messageBody = messageBody + htmlTdStart + table1.Rows[i][3] + htmlTdEnd;
+                    messageBody = messageBody + htmlTdStart + table1.Rows[i][4] + htmlTdEnd;
+                    messageBody = messageBody + htmlTdStart + table1.Rows[i][5] + htmlTdEnd;
+                    messageBody = messageBody + htmlTdStart + table1.Rows[i][6] + htmlTdEnd;
+                    messageBody = messageBody + htmlTdStart + table1.Rows[i][7] + htmlTdEnd;
+                    messageBody = messageBody + htmlTrEnd;
+                }
+
+                messageBody = messageBody + htmlTableEnd;
+                return messageBody;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
         protected void btnSend_Click(object sender, EventArgs e)
         {
             try
@@ -283,9 +348,7 @@ namespace Transport_Weekend
                         }
                     }
 
-                    GetUserName user = new GetUserName();
-                    string temp = user.GetName(Superior);
-                    Superior = temp;
+                   
 
                     if (SaturdayStatus == "PROGRAMMED")
                     {
@@ -381,6 +444,25 @@ namespace Transport_Weekend
 
 
                 }
+
+
+                //fill la datatable
+
+
+                //string Subiect;
+                //string Text;
+                //string Emailget;
+                //string Emailsend;
+
+                //Subiect = "HSE training status - for the people that are coming in the next 7 days";
+                //Text = getHtml(dt);
+                //Emailget = "vlad.arsene@marturfompak.com;ovidiu.gionea@marturfompak.com;";
+                //Emailsend = "cristian.nedelea@marturfompak.com";
+
+                //Email ema = new Email();
+
+                //ema.Send(Text, Subiect, Emailsend, Emailget);
+
                 DeleteTemp();
                 RefreshGridAll();
                 RefreshGridProgrammed();
